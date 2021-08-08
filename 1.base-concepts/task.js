da3ca5530433e0cb280f74827fe806d03c0e934b
Mariia Khudobina <masha.khudobina@mail.ru>
2021-08-08 20:57:53 +0300

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,26 @@
-function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+"use strict";
+
+  let arr = [
+    solveEquation = function(a,b,c) {
+      let d = (a, b, c) = Math.pow(b, 2) - 4 * a * c;
+      let x1, x2;
+       if (a==0) {
+         return "Ошибка"
+        };
+      if (d > 0) {
+      x1 = (-b - Math.sqrt(d))/ (2 * a);
+      x2 = (-b + Math.sqrt(d))/ (2 * a);
+      return  (x1, x2);
+      } else if (d === 0) {
+      x1 === -b/ (2 * a);
+      return (x1);  
+      } else if (d < 0) {
+      return ("корней нет") 
+      }
+   return solveEquation (2, 4, 3);
+    }
+   
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
@@ -10,4 +28,3 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   // код для задачи №2 писать здесь
 
   return totalAmount;
-}
