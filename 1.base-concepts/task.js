c789350ef5403da3ba1b7fb863171e0e9f146b61
Mariia Khudobina <masha.khudobina@mail.ru>
2021-08-21 21:19:43 +0300

    second commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,25 +1,28 @@
 "use strict";
 
-  let arr = [
+  
     solveEquation = function(a,b,c) {
-      let d = (a, b, c) = Math.pow(b, 2) - 4 * a * c;
+      let arr = [
+      let d = Math.pow(b, 2) - 4 * a * c;
       let x1, x2;
        if (a==0) {
          return "Ошибка"
         };
       if (d > 0) {
-      x1 = (-b - Math.sqrt(d))/ (2 * a);
-      x2 = (-b + Math.sqrt(d))/ (2 * a);
-      return  (x1, x2);
+      x1 = (-b + Math.sqrt(d))/ (2 * a);
+      return  (x1);
+      } else if {
+      x2 = (-b - Math.sqrt(d))/ (2 * a);
+      return  (x2);
       } else if (d === 0) {
-      x1 === -b/ (2 * a);
+      x1 = -b/ (2 * a);
       return (x1);  
       } else if (d < 0) {
-      return ("корней нет") 
+      return ''; 
       }
-   return solveEquation (2, 4, 3);
+   return arr;
     }
-   
+   console.log (x);
 
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
@@ -28,3 +31,4 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   // код для задачи №2 писать здесь
 
   return totalAmount;
+
